66b9c4ab164506030c08fac0e3f8c4aca3767309
gdarko <dg@darkog.com>
2020-03-06 13:17:04 +0100

    Update readme

--- spc-community-calendar.php
+++ spc-community-calendar.php
@@ -1,22 +1,10 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              https://stpetecatalyst.com
- * @since             1.0.2
- * @package           SPC_Community_Calendar
- *
- * @wordpress-plugin
  * Plugin Name:       SPC Community Calendar
  * Plugin URI:        https://stpetecatalyst.com
  * Description:       The catalyst events calendar
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Darko Gjorgjijoski (stpetecatalyst.com)
  * Author URI:        https://darkog.com
  * License:           GPL-2.0+
